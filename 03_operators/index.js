afe0a7ad6b8f2d15d2408a2f387379578957246f
ALI RAZA <aliraza61126112@gmail.com>
2026-04-27 07:08:54 -0700

    operators

---
+++ 03_operators/index.js
@@ -0,0 +1,40 @@
+//operator
+
+//arithmetic operator
+
+let a=100
+let b=200
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+
+//assignment operTOR
+
+let  x=55
+x**=x
+console.log(x)
+
+//comparison operator
+
+let c=10
+let t="10"
+console.log(c!==t)
+console.log(c!=t)
+
+//ternary operator 
+
+let age=18
+let reesult=age>=18?"you can vote":"you can not vote"
+console.log(reesult)
+
+let marks =33
+let mark=marks>=33?"you passed":"you failed"
+console.log(mark)
+
+//template litteral
+let name="ali"
+let rollno=100044
+let cgpa=3.3
+console.log(`your name is:: ${name} and your rollno is:: ${rollno} and your cgpa is ::
+    ${cgpa} `)
