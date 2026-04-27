f352cf70adc4768094b8b3f535e4bb854f14ddd7
ALI RAZA <aliraza61126112@gmail.com>
2026-04-27 08:23:46 -0700

    conditional part

---
+++ 04_conditiomnal/index.js
@@ -0,0 +1,99 @@
+// let greeting ;
+// let hour
+// if( hour==7){
+//     greeting="good mporning"
+   
+// }
+//  console.log(greeting)
+// let age =19
+ 
+    // else
+    // {
+    //     console.log("you can admission in school")
+    // }
+
+    // let adhar =true
+    // let pan =true
+    // if(adhar)
+    // {
+    //     console.log("you can open account in aour bank ")
+    // }
+    // else if(pan)
+    // {
+    //     console.log("you can open account in our bank")
+    // }
+    // else
+    // {
+    //     console.log("you cant open account in our bank")
+    // }
+
+    // let number=10
+    // if(number>5)
+    // {
+    //     console.log("number is great than 5")
+    // }
+    // else if(number>2)
+    // {
+    //     console.log("number is greater than 2 ")
+    // }
+    // else{
+    //     console.log("number is very less")
+    // }
+
+
+
+    // let day=1
+
+    // if(day==1)
+    // {
+    //     console.log("monday")
+    // }
+    // else if(day==2)
+    // {
+    //     console.log("tuesday")
+    // }
+    // else if(day==3)
+    // {
+    //     console.log("wedsday")
+    // }
+    
+    // else if(day==4)
+    // {
+    //     console.log("thursday")
+    // }
+    // else if(day==5)
+    // {
+    //     console.log("friday")
+    // }
+    // else if(day==6)
+    // {
+    //     console.log("saturday")
+    // }
+    // else
+    // {
+    //     console.log("sunday")
+    // }
+
+
+
+    // switch case
+
+    let day=18
+    switch(day){
+        case 1: console.log("monday")
+        break;
+        case 2: console.log("tuesday")
+             break;
+        case 3: console.log("wedsday")
+             break;
+        case 4: console.log("thursday")
+             break;
+        case 5: console.log("friday")
+             break;
+        case 6: console.log("saturday")
+             break;
+        case 7: console.log("sunday")
+             break;
+        default:
+            console.log("invalid number")
+    }
