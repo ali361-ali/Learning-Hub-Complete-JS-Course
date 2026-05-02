f1c04770aeeb49c65795b77a073cff40526919cf
ALI RAZA <aliraza61126112@gmail.com>
2026-05-02 08:12:13 -0700

    destructuring

---
+++ 09_destructuring/index.js
@@ -0,0 +1,20 @@
+console.log("we are learning destrucring in javascript")
+
+const smartphone={
+    name:"vivo",
+    camera:"128px",
+    baattery:"5000mah",
+    rom :"64gb",
+    ram:"4gb ",
+    charger:"5000mah"
+}
+// console.log(smartphone)
+
+// const smartphone2={
+//     ...smartphone,
+//     name:"galaxy"
+// }
+// console.log("smartphone 2 is ",smartphone2)
+
+const {rom,ram,name}=smartphone
+console.log(rom,ram,name)
