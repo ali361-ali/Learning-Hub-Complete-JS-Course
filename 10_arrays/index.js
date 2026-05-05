ced6c6f18ed78beb159fd7b9c0788021722b51c7
ALI RAZA <aliraza61126112@gmail.com>
2026-05-05 00:45:36 -0700

    I learn arrays in js

---
+++ 10_arrays/index.js
@@ -0,0 +1,17 @@
+console.log("we are learning array in javascript")
+
+let arr=["aliraza",100,4,4.4,"@"]
+let number =["ali",111,33,34,445,666,99
+]
+console.log(arr)
+console.log(number)
+//push
+
+//join
+
+
+
+
+
+
+
