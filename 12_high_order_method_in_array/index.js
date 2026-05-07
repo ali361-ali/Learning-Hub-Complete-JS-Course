97b43c00848d2a628d01947a1920b9a8add9c019
ALI RAZA <aliraza61126112@gmail.com>
2026-05-07 02:37:07 -0700

    high order method in array

---
+++ 12_high_order_method_in_array/index.js
@@ -0,0 +1,17 @@
+
+/// high order method
+//map method
+let ara=[1,2,3,4,5]
+let doubled=ara.map((el)=>el*9);
+console.log(doubled)
+
+//filter method
+
+let araa=[1,2,3,4,5,6,7,8,9]
+let even=araa.filter((e)=>e%2===0)
+console.log(even)
+
+//reduce
+let araaa=[1,2,3,4,5]
+let sum=araaa.reduce((prev,el)=>prev+el,8)
+console.log(sum)
