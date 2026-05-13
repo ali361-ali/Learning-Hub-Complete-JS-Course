0a4b03a8d7a969d1980d6999b68ef93aa7fde8af
ALI RAZA <aliraza61126112@gmail.com>
2026-05-13 11:02:58 -0700

    settime interval

---
+++ 17_setinterval_set_timeout/index.js
@@ -0,0 +1,11 @@
+console.log("we are learning setinterval in js")
+// setinterval  is used to excute a function repeatedly after a given function interval of time 
+
+// let intervalid=setInterval(() => {
+//     console.log("this runs every 3 second")
+// },7000);
+// // settimeout is used to only one time
+
+setTimeout(() => {
+    console.log("aliraza")
+}, 1000);
