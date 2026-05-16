d7423efc63b79ab9efe7a0b3b1bb4cbcda883a7b
ALI RAZA <aliraza61126112@gmail.com>
2026-05-16 11:58:01 -0700

    async function

---
+++ 19_sync_async/index.js
@@ -0,0 +1,13 @@
+//sync
+console.log("ali")
+console.log("raza")
+console.log("arfan")
+
+
+///async
+console.log("ali")
+    setTimeout(()=>
+    {
+        console.log("raza")
+  },3000)
+console.log("arfan")
