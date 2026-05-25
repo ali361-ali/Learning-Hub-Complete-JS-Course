8c89e4a1423dfe803bad50ad7ed0ea81cc6cae97
ALI RAZA <aliraza61126112@gmail.com>
2026-05-25 01:15:46 -0700

    dom manipulation

---
+++ 21_DOM_Manupolation/index.js
@@ -0,0 +1,14 @@
+let ele=document.querySelector('h1')
+console.log(ele)
+console.log("text content =",ele.textContent)
+console.log("inner html = ",ele.innerHTML)
+console.log("inner text =",ele.innerText)
+console.log("tag name = ",ele.tagName)
+console.log("class name = ",ele.className)
+
+
+//style 
+ele.style.color="red"
+ele.style.backgroundColor="green"
+ele.textContent="ali"
+ele.className="ali"
