db1e4e94fa509f37a920185ed2c420f84c6964d5
ALI RAZA <aliraza61126112@gmail.com>
2026-05-26 11:56:04 -0700

    dom types

---
+++ 22_dom_manupolation_types_attributes_properties/index.js
@@ -0,0 +1,22 @@
+let h2=document.createElement('div')
+
+h2.innerText="ali raza"
+
+
+let container=document.querySelector('.container')
+//container.append(h2)
+//container.prepend(h2)
+//container.before(h2)
+//container.after(h2)
+container.remove(h2)
+
+// h2.innerText="ali"
+// h2.setAttribute("class","raza")
+// h2.setAttribute("id","awais")
+
+// console.log(h2, h2.getAttribute("class"))
+
+
+// h2.removeAttribute("class")
+
+//  console.log(h2)
